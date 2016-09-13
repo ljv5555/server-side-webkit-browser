d1d961c76386bcaa8466be4bff85ef3e278b981e
ljv5555 <ljv5555@users.noreply.github.com>
2016-09-13 12:45:40 -0500

    Update btoa.js

--- src/main/resources/phantomjs/linux/examples/btoa.js
+++ src/main/resources/phantomjs/linux/examples/btoa.js
@@ -1,23 +1,13 @@
 // echoToFile.js - Write in a given file all the parameters passed on the CLI
 var fs = require('fs'),
-    system = require('system');
-
-if (system.args.length < 3) {
+    system = require('system'), page=require('webpage');
+var rtn = '';
+if (system.args.length > 1 ) {
     console.log("Usage: echoToFile.js DESTINATION_FILE <arguments to echo...>");
-    phantom.exit(1);
-} else {
-    var content = '',
-        f = null,
-        i;
-    for ( i= 2; i < system.args.length; ++i ) {
-        content += system.args[i] + (i === system.args.length-1 ? '' : ' ');
-    }
     
-    try {
-        fs.write(system.args[1], content, 'w');
-    } catch(e) {
-        console.log(e);
-    }
-
+} 
+    
+    console.log(''+rtn);
     phantom.exit();
+    
 }
